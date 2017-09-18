90d6ee20deea0220fab14ee79aa6b2497cd3722a
Danutz <danutzdar95@gmail.com>
2017-09-18 19:31:52 +0300

    options-page

--- wp-content/themes/appocalyptic/functions.php
+++ wp-content/themes/appocalyptic/functions.php
@@ -7,6 +7,55 @@
  * @package appocalyptic
  */
 
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	acf_add_options_sub_page('Header');
+	acf_add_options_sub_page('Footer');
+
+	acf_add_options_page(array(
+			'page_title' 	=> 'Theme Options',
+			'menu_title'	=> 'Theme Options',
+			'menu_slug' 	=> 'theme-options',
+			'capability'	=> 'edit_posts',
+			'parent_slug'   => '',
+			'position'      => false,
+			'icon_url'      => false,
+			'redirect'		=> false
+		));
+		
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Header',
+			'menu_title'	=> 'Header',
+			'menu_slug'	    => 'theme-options-header',
+			'capability'	=> 'edit_posts',
+			'parent_slug'   => 'theme-options',
+			'position'      => false,
+			'icon_url'      => false
+		));
+		
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Footer',
+			'menu_title'	=> 'Footer',
+			'menu_slug'	    => 'theme-options-footer',
+			'capability'	=> 'edit_posts',
+			'parent_slug'   => 'theme-options',
+			'position'      => false,
+			'icon_url'      => false
+		));
+		acf_add_options_sub_page(array(
+			'page_title' 	=> 'Post Settings',
+			'menu_title'	=> 'Post Settings',
+			'parent_slug'	=> 'post-settings',
+			'capability'	=> 'edit_posts',
+			'parent_slug'   => 'edit.php',
+			'position'      => false,
+			'icon_url'      => false
+		));
+	
+}
+
+ 
 if ( ! function_exists( 'appocalyptic_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
