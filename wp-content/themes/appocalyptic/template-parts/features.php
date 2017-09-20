2fc04e2c41206d846ffb3fb2f8609191ad8e9445
Johnny <ionutkjdlf@gmail.com>
2017-09-20 11:55:16 +0300

    "General changes to rows and boxes"

--- wp-content/themes/appocalyptic/template-parts/features.php
+++ wp-content/themes/appocalyptic/template-parts/features.php
@@ -7,46 +7,19 @@
  		<span class="fa-icon fa-phone"></span>
  	</div>	
  	<div class="row">
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
- 		</div>
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
- 		</div>
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
+ 		<div class="feature-box columns medium-4">
+ 		    <div class="feature-box-container">
+              <div class="icon-heading-container">
+                <div class="icon-heading">
+                </div>
+                <div class="icon-title"> 
+                  Lorem ipsum dolor.
+                </div>
+              </div>
+              <div class="section-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
+              </div>
+  			</div>
  		</div>
  	</div>
-
- 	<div class="row">
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
- 		</div>
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
- 		</div>
- 		<div class="columns medium-4">
- 			<div class="icon-heading">Lorem ipsum dolor.</div>
- 			<div class="section-description">
- 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nihil minima laudantium beatae, ea totam!</p>
- 			</div>
- 		</div>
- 	</div>
-
- 
  </div>
